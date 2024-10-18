1f916b70b46248202e9c31699b5b0cdcd59c3f22
iam_mamadou <momodouba44@gmail.com>
2024-10-18 15:41:32 +0000

    Add dynamic autoscaling policy

--- terraform/10-autoscaling.tf
+++ terraform/10-autoscaling.tf
@@ -11,6 +11,21 @@ resource "aws_autoscaling_group" "webserver" {
   
 }
 
+# Politique de scaling basée sur l'utilisation du CPU
+resource "aws_autoscaling_policy" "web_cpu_scaling" {
+  name                   = "cpu-scaling-policy"
+  adjustment_type         = "ChangeInCapacity"
+  autoscaling_group_name  = aws_autoscaling_group.webserver.name
+
+  policy_type             = "TargetTrackingScaling"
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 90.0  # Scalera si l'utilisation CPU dépasse 50%
+  }
+}
+
 resource "aws_autoscaling_group" "appserver" {
   name = "appserver-autoscaling"
   max_size = 5
@@ -23,3 +38,19 @@ resource "aws_autoscaling_group" "appserver" {
   target_group_arns = [aws_lb_target_group.app_tg.arn]
   
 }
+
+# Politique de scaling basée sur l'utilisation du CPU
+resource "aws_autoscaling_policy" "app_cpu_scaling" {
+  name                   = "cpu-scaling-policy"
+  adjustment_type         = "ChangeInCapacity"
+  autoscaling_group_name  = aws_autoscaling_group.appserver.name
+
+  policy_type             = "TargetTrackingScaling"
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 90.0  # Scalera si l'utilisation CPU dépasse 50%
+  }
+}
+
